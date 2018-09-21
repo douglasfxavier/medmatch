c1a605dc26ba2d93a1c4f4de2de8530711987a83
douglasfxavier <douglasf.xavier@gmail.com>
2018-09-20 23:15:51 -0300

    Altering the method of data update for file upload via HTTP request

--- src/main/java/bean/ConversionBean.java
+++ src/main/java/bean/ConversionBean.java
@@ -3,8 +3,6 @@ package bean;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
@@ -19,33 +17,15 @@ import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.jena.fuseki.embedded.FusekiServer;
-import org.apache.jena.fuseki.server.Operation;
-import org.apache.jena.query.Dataset;
-import org.apache.jena.query.DatasetFactory;
-import org.apache.jena.query.ReadWrite;
+import org.apache.jena.graph.Node;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdfconnection.RDFConnection;
-import org.apache.jena.rdfconnection.RDFConnectionFactory;
-import org.apache.jena.rdfconnection.RDFConnectionFuseki;
-import org.apache.jena.rdfconnection.RDFConnectionRemoteBuilder;
-import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.riot.RDFFormat;
-import org.apache.jena.riot.RIOT;
-import org.apache.jena.riot.WriterDatasetRIOT;
-import org.apache.jena.sparql.core.DatasetGraph;
-import org.apache.jena.system.Txn;
-import org.apache.jena.tdb2.TDB2Factory;
-import org.apache.jena.update.UpdateAction;
-import org.apache.jena.update.UpdateFactory;
-import org.apache.jena.update.UpdateRequest;
-import org.apache.jena.fuseki.servlets.*;
 
 import controller.ObjectsToRDFConverter;
 import controller.OntologyManager;
 import controller.RDFManager;
 import model.Country;
+import util.FusekiConnector;
+import util.SparqlUtil;
 
 @ManagedBean (name = "conversionBean")
 @SessionScoped
@@ -163,15 +143,15 @@ public class ConversionBean {
 	public void setCsvDataReaderBean(CSVDataReaderBean csvDataReaderBean) {
 		this.csvDataReaderBean = csvDataReaderBean;
 	}
-
+	
+	
+	
 	public String convert() throws NumberFormatException, FileNotFoundException, ArrayIndexOutOfBoundsException {
 		try {
 			Country country = this.matchingBean.getSelectedCountry();
-			String base = this.matchingBean.getUploadBean().getDatasetURL();
+			String baseURI = this.matchingBean.getUploadBean().getDatasetURL();
 			OntologyManager ontologyManager = this.matchingBean.getOntologyBean().getOntologyManager();
 			RDFManager manager = new RDFManager();
-			
-
 	
 			this.csvDataReaderBean.getCsvDataReader().loadData(country, 
 					   drugCodeIndex, 
@@ -182,60 +162,37 @@ public class ConversionBean {
 					   manufacturerNameIndex, 
 					   strengthIndex);
 	
-			ObjectsToRDFConverter rdfConverter = new ObjectsToRDFConverter(base, ontologyManager);
+			ObjectsToRDFConverter rdfConverter = new ObjectsToRDFConverter(baseURI, ontologyManager);
 			
 			this.rdfModel = rdfConverter.convertDataToRDF(this.csvDataReaderBean.getCsvDataReader());
-
-			FacesContext fc= FacesContext.getCurrentInstance();
-			
-			String path = fc.getExternalContext().getRealPath(String.format("WEB-INF\\classes\\data\\Drugs_%s.ttl",country.getCountryName().replace('\n', '_')));
-			manager.saveFile(rdfModel, path, "TURTLE",base);		
+						
+			FacesContext facesContext= FacesContext.getCurrentInstance();
+/*			String path = facesContext.getExternalContext().getRealPath(String.format("WEB-INF\\classes\\data\\Drugs_%s.ttl",country.getCountryName().replace('\n', '_')));
+			manager.saveFile(rdfModel, path, "TURTLE",base);	*/	
 		
-			path = fc.getExternalContext().getRealPath(String.format("WEB-INF\\classes\\data\\Drugs_%s.rdf",country.getCountryName().replace('\n', '_')));
-			manager.saveFile(rdfModel, path, "RDF/XML",base);			
-			
-			Dataset dataset = TDB2Factory.createDataset();
-			dataset.begin(ReadWrite.WRITE);
+			String path = facesContext.getExternalContext().getRealPath(String.format("WEB-INF\\classes\\data\\Drugs_%s.rdf",country.getCountryName().replace('\n', '_')));
+			manager.saveFile(rdfModel, path, "RDF/XML",baseURI);
 
+/*			SparqlUtil sparqlUtil = new SparqlUtil();
+			String fusekiService = "http://localhost:3030/drugs_brazil/update";
+			sparqlUtil.insertTriples(rdfModel, baseURI, fusekiService);*/
+			
 			File file = new File(path);
 			FileBody fileBody = new FileBody(file,ContentType.DEFAULT_BINARY);
 			
 			MultipartEntityBuilder builder = MultipartEntityBuilder.create();
 		        builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
 		        builder.addPart("file", fileBody);
-		        builder.setContentType(ContentType.create("application/rdf+xml"));
 		        HttpEntity entity = builder.build();
-		        HttpPost request = new HttpPost("http://localhost:8082/fuseki/medmatch/upload");
+		        HttpPost request = new HttpPost("http://localhost:3030/drugs_brazil/upload");
 		        request.setEntity(entity);
-		       
-
+		        
 		        HttpClient client = HttpClientBuilder.create().build();
 		        try {
 		            client.execute(request);
 		        } catch (IOException e) {
 		            e.printStackTrace();
 		        }
-			
-			/*			FusekiServer server = FusekiServer.create()
-				    .add("/medmatch", dataset, true)
-				    .addOperation("/medmatch", 
-				    		"http://localhost:8082/fuseki/medmatch/update", Operation.Update)
-				    .build();
-			server.start();
-			
-			UpdateRequest request = UpdateFactory.create();
-			
-			request.add("DROP ALL")
-				   .add("PREFIX dc: <http://purl.org/dc/elements/1.1/>\r\n" + 
-				   		"INSERT DATA\r\n" + 
-				   		"{ \r\n" + 
-				   		"  <http://example/book1> dc:title \"A new book\" ;\r\n" + 
-				   		"                         dc:creator \"A.N.Other\" .\r\n" + 
-				   		"}");
-				   			
-			
-			server.stop();*/
-
 			return "conversion?faces-redirect=true";
 		}catch (Exception e) {
 			e.printStackTrace();
